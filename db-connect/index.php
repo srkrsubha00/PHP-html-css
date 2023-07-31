b1be72366176ea9dd357aab00f9d76c7a83f3ef5
Subhajit Sarkar <56867899+srkrsubha00@users.noreply.github.com>
2023-07-31 16:39:33 +0530

    Add files via upload

---
+++ db-connect/index.php
@@ -0,0 +1,15 @@
+<?php
+    include("database.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My website</title>
+</head>
+<body>
+    hello<br>
+</body>
+</html>
